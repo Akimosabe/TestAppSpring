65d97dde89749ba47f4668949fdb2dd3385b8048
Akimosabe <he7seven@gmail.com>
2023-09-17 16:18:23 +0500

    Initial commit

---
+++ src/main/java/ru/bogdanov/TestAppSpring/TestAppSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.TestAppSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bogdanov/TestAppSpring/TestAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.TestAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
