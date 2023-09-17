1cdc566b1b1fdec909cf6bbd601234d4a4324a7f
Akimosabe <he7seven@gmail.com>
2023-09-17 17:00:05 +0500

    add /update-array

--- src/main/java/ru/bogdanov/TestAppSpring/hello/HelloContoller.java
+++ src/main/java/ru/bogdanov/TestAppSpring/hello/HelloContoller.java
@@ -5,9 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 
 public class HelloContoller {
-    @GetMapping("/hello")    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);    }
+
+    private List<String> array = new ArrayList<>();
+    @GetMapping("/update-array")
+    public String updateArrayList(@RequestParam(value = "array", defaultValue = "") String sense) {
+        if (array.isEmpty()) {
+            array = new ArrayList<>();
+        }
+        array.add(sense);
+
+        return array + " has been successfully added to the list";
+            }
+
 }
